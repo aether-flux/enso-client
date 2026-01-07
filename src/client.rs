28100077f0a4179113584b122d845747d79ad67d
aether-flux <work.amar05@yahoo.com>
2026-01-08 01:19:11 +0530

    fix: fixed hanged response

--- src/client.rs
+++ src/client.rs
@@ -1,28 +1,40 @@
-use std::{io::{BufRead, BufReader, Write}, net::TcpStream};
+use std::{io::{BufRead, BufReader, BufWriter, Write}, net::TcpStream};
 
 use crate::{error::{EnsoError}, protocol::EOF_MARKER};
 
 pub struct Enso {
     reader: BufReader<TcpStream>,
+    writer: BufWriter<TcpStream>,
 }
 
 impl Enso {
     pub fn connect(addr: &str) -> std::io::Result<Self> {
         let stream = TcpStream::connect(addr)?;
-        let reader = BufReader::new(stream);
-        Ok(Self { reader })
+        stream.set_nodelay(true)?;
+
+        let reader = BufReader::new(stream.try_clone()?);
+        let writer = BufWriter::new(stream);
+        Ok(Self { reader, writer })
     }
 
     pub fn execute(&mut self, query: &str) -> Result<String, EnsoError> {
-        self.reader.get_mut().write_all(query.as_bytes())?;
-        self.reader.get_mut().write_all(b"\n")?;
+        self.writer.write_all(query.as_bytes())?;
+        self.writer.write_all(b"\n")?;
+        self.writer.flush()?;
+
+        // self.reader.get_mut().write_all(query.as_bytes())?;
+        // self.reader.get_mut().write_all(b"\n")?;
 
         let mut response = String::new();
         // self.reader.read_line(&mut response)?;
 
         loop {
             let mut line = String::new();
-            self.reader.read_line(&mut line)?;
+            let bytes = self.reader.read_line(&mut line).unwrap_or(0);
+
+            if bytes == 0 {
+                break;
+            }
 
             if line.trim() == EOF_MARKER {
                 break;
